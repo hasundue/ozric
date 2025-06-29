12266d6f7059f265289c727f83150a85e9605f44
Shun Ueda <hasundue@gmail.com>
2025-06-29 22:39:43 +0900

    chore: init with zig2nix template

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
-  description = "Integral equation thery in Zig";
+  description = "An OZ equation solver using ceres-solver, cross-compiled with Zig";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    git-hooks-nix = {
+    zig2nix = {
+      url = "github:Cloudef/zig2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    githooks-nix = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -16,79 +20,116 @@
 
   outputs =
     {
-      self,
       nixpkgs,
-      git-hooks-nix,
+      zig2nix,
+      githooks-nix,
       treefmt-nix,
+      ...
     }:
     let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSystem =
-        f:
-        nixpkgs.lib.genAttrs systems (
-          system:
-          f {
-            inherit system;
-            pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = false;
-            };
-            lib = nixpkgs.lib // {
-              git-hooks-nix = git-hooks-nix.lib;
-              treefmt-nix = treefmt-nix.lib;
-            };
-          }
-        );
+      flake-utils = zig2nix.inputs.flake-utils;
     in
-    {
-      devShells = forEachSystem (
-        {
-          pkgs,
-          lib,
-          system,
-        }:
-        let
-          treefmt = lib.treefmt-nix.mkWrapper pkgs {
-            programs.nixfmt = {
-              enable = true;
-            };
-          };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-          git-hooks = lib.git-hooks-nix.${system}.run {
-            src = ./.;
-            hooks = {
-              treefmt = {
+        lib = nixpkgs.lib // {
+          githooks-nix = githooks-nix.lib;
+          treefmt-nix = treefmt-nix.lib;
+        };
+
+        # Zig flake helper
+        # Check the flake.nix in zig2nix project for more options:
+        # <https://github.com/Cloudef/zig2nix/blob/master/flake.nix>
+        env = zig2nix.outputs.zig-env.${system} { };
+      in
+      with builtins;
+      with env.pkgs.lib;
+      rec {
+        # Produces clean binaries meant to be ship'd outside of nix
+        # nix build .#foreign
+        packages.foreign = env.package {
+          src = cleanSource ./.;
+
+          # Packages required for compiling
+          nativeBuildInputs = with env.pkgs; [ ];
+
+          # Packages required for linking
+          buildInputs = with env.pkgs; [ ];
+
+          # Smaller binaries and avoids shipping glibc.
+          zigPreferMusl = true;
+        };
+
+        # nix build .
+        packages.default = packages.foreign.override (attrs: {
+          # Prefer nix friendly settings.
+          zigPreferMusl = false;
+
+          # Executables required for runtime
+          # These packages will be added to the PATH
+          zigWrapperBins = with env.pkgs; [ ];
+
+          # Libraries required for runtime
+          # These packages will be added to the LD_LIBRARY_PATH
+          zigWrapperLibs = attrs.buildInputs or [ ];
+        });
+
+        # For bundling with nix bundle for running outside of nix
+        # example: https://github.com/ralismark/nix-appimage
+        apps.bundle = {
+          type = "app";
+          program = "${packages.foreign}/bin/default";
+        };
+
+        # nix run .
+        apps.default = env.app [ ] "zig build run -- \"$@\"";
+
+        # nix run .#build
+        apps.build = env.app [ ] "zig build \"$@\"";
+
+        # nix run .#test
+        apps.test = env.app [ ] "zig build test -- \"$@\"";
+
+        # nix run .#docs
+        apps.docs = env.app [ ] "zig build docs -- \"$@\"";
+
+        # nix run .#zig2nix
+        apps.zig2nix = env.app [ ] "zig2nix \"$@\"";
+
+        # nix develop
+        devShells.default =
+          let
+            treefmt = lib.treefmt-nix.mkWrapper pkgs {
+              programs.nixfmt.enable = true;
+            };
+            githooks = lib.githooks-nix.${system}.run {
+              src = ./.;
+              hooks.treefmt = {
                 enable = true;
                 package = treefmt;
               };
             };
-          };
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              treefmt
-              # Add your development dependencies here
-            ];
+          in
+          env.mkShell {
+            # Packages required for compiling, linking and running
+            # Libraries added here will be automatically added to the LD_LIBRARY_PATH and PKG_CONFIG_PATH
+            nativeBuildInputs =
+              [ ]
+              ++ packages.default.nativeBuildInputs
+              ++ packages.default.buildInputs
+              ++ packages.default.zigWrapperBins
+              ++ packages.default.zigWrapperLibs;
+
+            # Packages required for development
             packages = with pkgs; [
               nil
+              treefmt
               zls
             ];
-            shellHook = git-hooks.shellHook;
+            shellHook = githooks.shellHook;
           };
-        }
-      );
-
-      packages = forEachSystem (
-        { pkgs, ... }:
-        {
-          default = pkgs.hello; # Replace with your package
-        }
-      );
-    };
+      }
+    );
 }
