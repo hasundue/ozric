20cef3d2a940f072492ec89cf3ba8ed933b4c68a
Shun Ueda <hasundue@gmail.com>
2025-07-04 13:00:42 +0900

    refactor: remove zig wrappers of c++ functions

--- src/root.zig
+++ src/root.zig
@@ -5,29 +5,13 @@ const c = @cImport({
     @cInclude("solver.h");
 });
 
-pub fn getCeresVersion() i32 {
-    return c.test_ceres();
-}
-
-pub fn runHelloWorld() f64 {
-    return c.run_hello_world();
-}
-
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
-
-test "ozric main functionality" {
-    const ceres_version = getCeresVersion();
+test "solver" {
+    const ceres_version = c.test_ceres();
 
     std.debug.print("Ozric - Ornstein-Zernike equation solver\n", .{});
     std.debug.print("Using Ceres Solver version: {d}\n", .{ceres_version});
 
-    const result = runHelloWorld();
+    const result = c.run_hello_world();
     std.debug.print("Hello World result: x = {d} (should be ~10.0)\n", .{result});
 
     try testing.expectApproxEqAbs(10.0, result, 1e-4);
