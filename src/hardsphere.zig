59faa7623bc59e65ab3c8c369bc8656b3ca3d365
Shun Ueda <hasundue@gmail.com>
2025-07-15 23:20:05 +0900

    fmt

--- src/hardsphere.zig
+++ src/hardsphere.zig
@@ -20,10 +20,8 @@ pub const HardSphereDFT = struct {
     size: usize,
 
     /// Descritized weight functions over the grid
+    /// [0] = w_3(z), [1] = w_2(z), [2] = w^i_2(z)
     weights: [3][]f64,
-
-    /// Effective weight functions for 1D integration
-    weights_eff: [3][]f64,
 
     const Self = @This();
 
@@ -37,52 +35,35 @@ pub const HardSphereDFT = struct {
         const size = GRID_PPD;
         const resolution: f64 = diameter / @as(f64, size);
 
-        const weight_fns = .{ weightFn0, weightFn1, weightFn2 };
-
         var weights: [3][]f64 = undefined;
         inline for (0..3) |i| {
-            const support = if (i == 1) 2 else 1;
-            weights[i] = try allocator.alloc(f64, size * support + 1);
-            for (0..weights[i].len) |j| {
-                const r = @as(f64, @floatFromInt(j)) * resolution;
-                weights[i][j] = weight_fns[i](diameter, r);
-            }
+            weights[i] = try allocator.alloc(f64, size + 1);
         }
-
-        var weights_eff: [3][]f64 = undefined;
-        inline for (0..3) |i| {
-            weights_eff[i] = try allocator.alloc(f64, weights[i].len);
-            @memset(weights_eff[i], 0.0);
-            for (0..weights_eff[i].len) |j| {
-                const r_0: f64 = @as(f64, @floatFromInt(j)) * resolution;
-                for (0..weights[i].len) |k| {
-                    // TODO: Replace with Simpson integration
-                    const u = @as(f64, @floatFromInt(k)) * resolution;
-                    const r = math.pow(f64, r_0, 2) + u;
-                    weights_eff[i][j] += weight_fns[i](diameter, math.sqrt(r));
-                }
-                weights_eff[i][j] *= math.pi * resolution;
-            }
+        for (0..size + 1) |i| {
+            const i_ = @as(f64, @floatFromInt(i));
+            const z = i_ * resolution;
+            const x = i_ / @as(f64, size);
+            weights[0][i] = math.pi * diameter * diameter * (1.0 - x * x);
+            weights[2][i] = 2.0 * math.pi * z;
         }
+        @memset(weights[1], 2.0 * math.pi * diameter);
 
         return Self{
             .diameter = diameter,
             .resolution = resolution,
             .size = size,
             .weights = weights,
-            .weights_eff = weights_eff,
         };
     }
 
     pub fn deinit(self: Self, allocator: std.mem.Allocator) void {
         for (0..3) |i| {
             allocator.free(self.weights[i]);
-            allocator.free(self.weights_eff[i]);
         }
     }
 };
 
-test "HardSphereDFT init" {
+test "HardSphereDFT.init" {
     const hs = try HardSphereDFT.init(t.allocator, 1.0);
     defer hs.deinit(t.allocator);
 
@@ -90,23 +71,14 @@ test "HardSphereDFT init" {
     try t.expectEqual(1.0 / 16.0, hs.resolution);
     try t.expectEqual(16, hs.size);
     try t.expectEqual(16 + 1, hs.weights[0].len);
-    try t.expectEqual(32 + 1, hs.weights[1].len);
+    try t.expectEqual(16 + 1, hs.weights[1].len);
     try t.expectEqual(16 + 1, hs.weights[2].len);
-    try t.expectEqual(16 + 1, hs.weights_eff[0].len);
-    try t.expectEqual(32 + 1, hs.weights_eff[1].len);
-    try t.expectEqual(16 + 1, hs.weights_eff[2].len);
-
-    std.debug.print("weights_eff[0]: {any}\n", .{hs.weights_eff[0]});
-    std.debug.print("weights_eff[1]: {any}\n", .{hs.weights_eff[1]});
-    std.debug.print("weights_eff[2]: {any}\n", .{hs.weights_eff[2]});
 }
 
 const Kernels = struct {
     /// Convolution kernels for the weight functions
+    /// [0] = w3(z), [1] = w2(z), [2] = v2(z)
     weights: [3]conv.RadialKernel,
-
-    /// Convolution kernel for the excess chemical potential term
-    // excess: conv.RadialKernel,
 
     const Self = @This();
 
@@ -116,14 +88,14 @@ const Kernels = struct {
             const simpson_weights = try conv.RadialWeights.init(
                 .simpson,
                 allocator,
-                hs.weights_eff[i].len,
+                hs.weights[i].len,
                 grid.spacing,
             );
             defer simpson_weights.deinit(allocator);
 
             weights[i] = try conv.RadialKernel.init(
                 allocator,
-                hs.weights_eff[i],
+                hs.weights[i],
                 grid.points.len,
                 simpson_weights,
             );
@@ -138,7 +110,7 @@ const Kernels = struct {
     }
 };
 
-test "WeightIntegral init" {
+test "Kernels.init" {
     const allocator = t.allocator;
 
     const hs = try HardSphereDFT.init(allocator, 1.0);
@@ -152,37 +124,32 @@ test "WeightIntegral init" {
 }
 
 const WeightedDensity = struct {
-    density: []f64,
-    expansions: [3][]f64,
+    data: []f64,
+    grid: Grid,
 
     const Self = @This();
 
     pub fn init(allocator: std.mem.Allocator, grid: Grid) !Self {
         const n = grid.points.len;
-        var expansion: [3][]f64 = undefined;
-        for (0..3) |i| expansion[i] = try allocator.alloc(f64, n);
         return Self{
-            .density = try allocator.alloc(f64, n),
-            .expansions = expansion,
+            .data = try allocator.alloc(f64, 3 * n),
+            .grid = grid,
         };
     }
 
     pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
-        allocator.free(self.density);
-        for (0..3) |i| allocator.free(self.expansions[i]);
+        allocator.free(self.data);
     }
 
     pub fn update(
         self: Self,
         density: []const f64,
-        integral: Kernels,
+        kernels: Kernels,
     ) void {
-        // const n = self.density.len;
-        // const size = integral.kernels[0].size;
-
-        inline for (0..3) |i| {
-            integral.weights[i].convolve(density, self.expansions[i]);
-        }
+        const n = density.len;
+        kernels.weights[0].convolve(density, self.data[0..n]);
+        kernels.weights[1].convolve(density, self.data[n .. 2 * n]);
+        kernels.weights[2].convolve(density, self.data[2 * n .. 3 * n]);
     }
 };
 
@@ -194,6 +161,7 @@ test "WeightedDensity" {
 
     const grid = try Grid.init(allocator, dft.resolution, 5.0);
     defer grid.deinit();
+    const n = grid.points.len;
     const center = grid.points.len / 2 - 1;
 
     const integral = try Kernels.init(allocator, dft, grid);
@@ -202,178 +170,13 @@ test "WeightedDensity" {
     var weighted = try WeightedDensity.init(allocator, grid);
     defer weighted.deinit(allocator);
 
-    try t.expectEqual(grid.points.len, weighted.density.len);
-    for (0..3) |i| {
-        try t.expectEqual(grid.points.len, weighted.expansions[i].len);
-    }
-
     const density = try allocator.alloc(f64, grid.points.len);
     defer allocator.free(density);
-    @memset(density, 0.57);
+    @memset(density, 1.0);
 
     weighted.update(density, integral);
 
-    std.debug.print("w[0]: {}\n", .{weighted.expansions[0][center]});
-    std.debug.print("w[1]: {}\n", .{weighted.expansions[1][center]});
-    std.debug.print("w[2]: {}\n", .{weighted.expansions[2][center]});
-}
-
-pub const HardSphereWorkspace = struct {
-    /// The smoothed (weighted) density profile
-    weighted_density: []f64,
-
-    /// The functional derivative of the smoothed density profile with respect to density
-    weighted_density_derivative: []f64,
-
-    /// The expansion coefficients of the smoothed (weighted) density profiles (array of 3 vectors)
-    weighted_density_expansions: [3][]f64,
-
-    /// Fourier-transformed density profile (complex vector)
-    density_fourier: []math.Complex(f64),
-
-    /// Fourier-transformed results for the weighted densities (complex vector)
-    weighted_density_fourier: []math.Complex(f64),
-
-    /// Fourier-transformed expansion coefficients
-    weight_fns_fourier: [3][]math.Complex(f64),
-
-    /// Allocator used for memory management
-    allocator: std.mem.Allocator,
-
-    /// Reference to the grid
-    grid: Grid,
-
-    /// Reference to the hard sphere DFT
-    hs: HardSphereDFT,
-
-    const Self = @This();
-
-    /// Initialize the density fnal workspace buffers
-    pub fn init(allocator: std.mem.Allocator, grid: Grid, hs: HardSphereDFT) !Self {
-        var weighted_density_expansions: [3][]f64 = undefined;
-        var weight_fns_fourier: [3][]math.Complex(f64) = undefined;
-
-        const n = grid.points.len;
-        const fft_size = grid.getFftSize();
-
-        // Allocate memory for each array in weighted_density_expansions
-        for (0..3) |i| {
-            weighted_density_expansions[i] = try allocator.alloc(f64, n);
-        }
-
-        // Allocate memory for Fourier-transformed weights
-        for (0..3) |i| {
-            weight_fns_fourier[i] = try allocator.alloc(math.Complex(f64), fft_size);
-        }
-
-        return Self{
-            .weighted_density = try allocator.alloc(f64, n),
-            .weighted_density_derivative = try allocator.alloc(f64, n),
-            .weighted_density_expansions = weighted_density_expansions,
-            .density_fourier = try allocator.alloc(math.Complex(f64), fft_size),
-            .weighted_density_fourier = try allocator.alloc(math.Complex(f64), fft_size),
-            .weight_fns_fourier = weight_fns_fourier,
-            .allocator = allocator,
-            .grid = grid,
-            .hs = hs,
-        };
-    }
-
-    /// Deinitialize and free allocated memory
-    pub fn deinit(self: *Self) void {
-        {
-            self.allocator.free(self.weighted_density);
-            self.allocator.free(self.weighted_density_derivative);
-
-            for (0..3) |i| {
-                self.allocator.free(self.weighted_density_expansions[i]);
-            }
-
-            for (0..3) |i| {
-                self.allocator.free(self.weight_fns_fourier[i]);
-            }
-
-            self.allocator.free(self.density_fourier);
-            self.allocator.free(self.weighted_density_fourier);
-        }
-    }
-};
-
-test "HardSphereWorkspace init" {
-    const allocator = t.allocator;
-
-    const hs = try HardSphereDFT.init(allocator, 1.0);
-    defer hs.deinit(allocator);
-
-    var grid = try Grid.init(allocator, hs.resolution, 5.0);
-    defer grid.deinit();
-
-    var workspace = try HardSphereWorkspace.init(allocator, grid, hs);
-    defer workspace.deinit();
-}
-
-fn weightFn0(sigma: f64, r: f64) f64 {
-    if (r > sigma) return 0.0;
-
-    const sigma3 = math.pow(f64, sigma, 3);
-    const pi = math.pi;
-
-    return 3 / (4 * pi * sigma3);
-}
-
-fn weightFn1(sigma: f64, r: f64) f64 {
-    const x = r / sigma;
-    const x2 = math.pow(f64, x, 2);
-    const x3 = math.pow(f64, x, 3);
-
-    if (r <= sigma) {
-        const a_0 = 0.90724;
-        const a_1 = -1.23717;
-        const a_2 = 0.21616;
-
-        return a_0 + a_1 * x + a_2 * x2;
-    } else {
-        const c = -0.10244;
-        const b_0 = 35.134;
-        const alpha = 4.934;
-        const b_1 = -98.684;
-        const beta_1 = 3.5621;
-        const b_2 = 92.693;
-        const beta_2 = 12.0;
-        const b_3 = -29.257;
-
-        const first = c * @exp(-beta_1 * (x - 1.0)) * @sin(alpha * (x - 1.0));
-        const second = @exp(-beta_2 * (x - 1.0)) * (b_0 + b_1 * x + b_2 * x2 + b_3 * x3);
-
-        return first + second;
-    }
-}
-
-test "weightFn1" {
-    try t.expectApproxEqAbs(0.90724, weightFn1(1.0, 0), 1e-5);
-
-    // Should be positive deep inside the sphere
-    try t.expect(weightFn1(1.0, 0.5) > 0);
-
-    // Should be negative at contact distance
-    try t.expect(weightFn1(1.0, 1.0) < 0);
-
-    // Test continuity at contact distance
-    const eps = 1e-6;
-    try t.expectApproxEqAbs(weightFn1(1.0, 1.0 - eps), weightFn1(1.0, 1.0 + eps), 1e-3);
-
-    // Test oscillating structure of the tail
-    try t.expect(weightFn1(1.0, 1.5) < 0);
-    try t.expect(weightFn1(1.0, 2.0) > 0);
-    try t.expect(weightFn1(1.0, 2.5) < 0);
-}
-
-fn weightFn2(sigma: f64, r: f64) f64 {
-    if (r > sigma) return 0.0;
-
-    const x = r / sigma;
-    const x2 = x * x;
-    const pi: f64 = math.pi;
-
-    return 5 / (4 * pi) * (6 - 12 * x + 5 * x2);
+    std.debug.print("w[0]: {}\n", .{weighted.data[center]});
+    std.debug.print("w[1]: {}\n", .{weighted.data[n + center]});
+    std.debug.print("w[2]: {}\n", .{weighted.data[2 * n + center]});
 }
